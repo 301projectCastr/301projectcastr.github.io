fd7fe64d920e58358a434aa0e9f957b1a0c70ea5
Carl Olson <colson10@gmail.com>
2018-03-05 17:21:53 -0800

    route js outline

---
+++ views/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// page('/', ctx => ); // main view logged in or not.
+// page('/mon/new', ctx => );  // create new pokemon.
+// page('/mon/:mon_id/', ctx => ); // detail view after creating new.
+// page('/mon/:mon_id/delete', ctx => ); // delete pokemon from user profile/db.
+// page('/pick_fight', ctx => ); // pick fight page.
+// page('/fight', ctx => ); // fight page.
+
+// page();
+
