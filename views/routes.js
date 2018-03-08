b15040c3e1a6e0b9ca5be8b5effbedc15a218145
Kevin <kevnaf@gmail.com>
2018-03-07 21:54:36 -0800

    EOD

--- views/routes.js
+++ views/routes.js
@@ -3,8 +3,6 @@
 page('/', ctx => app.monView.checkLocalStorage()); // main view logged in or not.
 page('/mon/new', ctx => app.monView.initNewMon());  // create new pokemon.
 page('/logout', ctx => app.monView.logout());
-// page('/mon/new', ctx => );  // create new pokemon.
-// page('/mon/:mon_id/', ctx => ); // detail view after creating new.
 // page('/mon/:mon_id/delete', ctx => ); // delete pokemon from user profile/db.
 // page('/pick_fight', ctx => ); // pick fight page.
 // page('/fight', ctx => ); // fight page.
